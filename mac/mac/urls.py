d3c890c6dac61002848224b340cc892a3509cc76
Pyush146 <65114644+Pyush146@users.noreply.github.com>
2021-09-21 13:33:08 +0530

    Update urls.py

--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/',include("shop.urls")),
     
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
